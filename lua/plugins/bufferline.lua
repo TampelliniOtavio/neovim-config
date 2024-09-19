9bbe86b64efd2720939bf0b3b0aec603b3941a02
Otavio Tampellini <tampelliniotavio@gmail.com>
2024-09-19 10:29:35 -0300

    Add Indicators on tabs

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,5 +2,28 @@ return {
 	"akinsho/bufferline.nvim",
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
-	opts = {},
+	opts = {
+		options = {
+			diagnostics = "nvim_lsp",
+			themable = true,
+			offsets = {
+				{
+					filetype = "NvimTree",
+				},
+			},
+			diagnostics_indicator = function(count, level, diagnostics_dict, context)
+				local s = " "
+				for e, n in pairs(diagnostics_dict) do
+					local sym = ""
+					if e == "error" then
+						sym = " "
+					elseif e == "warning" then
+						sym = " "
+					end
+					s = s .. n .. sym
+				end
+				return s
+			end,
+		},
+	},
 }
