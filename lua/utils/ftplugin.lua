22529c76f79a5f0cd0d025efa60420abd9a096c6
Otavio Tampellini <tampelliniotavio@gmail.com>
2025-04-30 09:39:06 -0300

    Add const for tab or space on setTabWidth

--- lua/utils/ftplugin.lua
+++ lua/utils/ftplugin.lua
@@ -1,14 +1,18 @@
 local M = {}
 
+M.TAB = ""
+
+M.SPACE = "expandtab"
+
 ---@param tab_size number
 ---@param expandtab? boolean
 M.setTabWidth = function(tab_size, expandtab)
 	expandtab = expandtab == nil and true or expandtab
 
-	local expand = ""
+	local expand = M.TAB
 
 	if expandtab then
-		expand = " expandtab"
+		expand = " " .. M.SPACE
 	end
 
 	vim.cmd("setlocal tabstop=" .. tab_size .. " shiftwidth=" .. tab_size .. expand)
