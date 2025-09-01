0e5194071367d84ca94e1eba35b9297dc152ea8f
Otavio Tampellini <tampelliniotavio@gmail.com>
2025-09-01 16:21:36 -0300

    Add expandtab by default

--- lua/defaults.lua
+++ lua/defaults.lua
@@ -3,6 +3,7 @@ vim.opt.smarttab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.termguicolors = true
+vim.o.expandtab = true
 
 -- Using System Clipboard
 vim.opt.clipboard = "unnamedplus"
