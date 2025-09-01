89806191a686b1b2ccc0bcc4faf125df79059dcc
Otavio Tampellini <tampelliniotavio@gmail.com>
2025-09-01 16:21:10 -0300

    Add emmet language server

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -12,6 +12,7 @@ return {
 				"jsonls",
 				"yamlls",
 				"gopls",
+				"emmet_language_server",
 			},
 			automatic_installation = true,
 		},
@@ -62,6 +63,48 @@ return {
 					vue = {
 						hybridMode = false,
 					},
+				},
+			})
+
+			lspconfig.emmet_language_server.setup({
+				filetypes = {
+					"css",
+					"eruby",
+					"html",
+					"javascript",
+					"javascriptreact",
+					"less",
+					"sass",
+					"scss",
+					"pug",
+					"typescriptreact",
+					"blade",
+				},
+				-- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+				-- **Note:** only the options listed in the table are supported.
+				init_options = {
+					---@type table<string, string>
+					includeLanguages = {},
+					--- @type string[]
+					excludeLanguages = {},
+					--- @type string[]
+					extensionsPath = {},
+					--- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+					preferences = {
+						filter = {
+							commentAfter = "",
+						},
+					},
+					--- @type boolean Defaults to `true`
+					showAbbreviationSuggestions = true,
+					--- @type "always" | "never" Defaults to `"always"`
+					showExpandedAbbreviation = "always",
+					--- @type boolean Defaults to `false`
+					showSuggestionsAsSnippets = false,
+					--- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+					syntaxProfiles = {},
+					--- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+					variables = {},
 				},
 			})
 		end,
